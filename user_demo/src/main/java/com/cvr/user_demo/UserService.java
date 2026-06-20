378ef3a8c394e78b7a547c3b2203067d460d73d4
CNU2005 <usrinivasulu54@gmail.com>
2026-06-20 13:26:16 +0530

    Spring Boot project (user_demo)

---
+++ user_demo/src/main/java/com/cvr/user_demo/UserService.java
@@ -0,0 +1,27 @@
+package com.cvr.user_demo;
+
+import java.util.*;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+	private UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	@Transactional
+	public void save(User user) {
+		userRepository.save(user);
+	}
+	public Optional<User> find(int id)
+	{
+		return userRepository.findById(id);
+	}
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+}
